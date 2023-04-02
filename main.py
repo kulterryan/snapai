ccdaeb1863a480c6fa5d9703b980eef1ee34d313
Aryan Chaurasia <aryan6647@gmail.com>
2023-04-03 00:07:42 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,34 @@
+"""
+# Minor Project
+Project Name: SnapAI
+Team Members:
+    1. Anushka Prajapati
+    2. Aryan Chaurasia
+    3. Pratyush Dixit
+
+Project Description:
+
+Project Features:
+
+Project Dependencies:
+
+Project Files:
+
+"""
+
+# Importing the required libraries
+
+# Main
+def main():
+  print("# Minor Project")
+  print("Choose one of the following options:")
+  print("1. Voice to Image")
+  print("2. Text to Image")
+  print("3. Image to Text")
+  print("4. Image Restoration")
+  opt_input = int(input("Enter your choice: "))
+  print("You have chosen option: ", opt_input)
+  print("Hello World!")
+
+if __name__ == "__main__":
+  main()
