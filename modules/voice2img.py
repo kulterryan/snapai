31df0b9b8c429f61ecce0c29004e0d666a4d7be5
Aryan Chaurasia <aryan6647@gmail.com>
2023-04-06 14:20:49 +0530

    Updated Modules

--- modules/voice2img.py
+++ modules/voice2img.py
@@ -2,6 +2,16 @@
 Voice 2 Image Generator Module
 """
 
-def voice2img():
-  print("Voice to Image Converter")
+# Importing Libraries
+import voice2text
+
+def voice2img(inp_voice):
+  print("HIT: Voice to Image Generator")
+  print("Input Voice: ", inp_voice)
   
+def main():
+  inp_voice = voice2text.voice2text()
+  voice2img(inp_voice)
+
+if __name__ == "__main__":
+  main()
--- modules/voice2text.py
+++ modules/voice2text.py
@@ -6,24 +6,25 @@ import torch
 
 model = whisper.load_model("base")
 
-# load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio("recordings/recording.mp3")
-audio = whisper.pad_or_trim(audio)
+def voice2text():
+  print("HIT: Voice to Text Generator")
+  # load audio and pad/trim it to fit 30 seconds
+  audio = whisper.load_audio("recordings/recording.mp3")
+  audio = whisper.pad_or_trim(audio)
 
-# make log-Mel spectrogram and move to the same device as the model
-mel = whisper.log_mel_spectrogram(audio).to(model.device)
+  # make log-Mel spectrogram and move to the same device as the model
+  mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-# detect the spoken language
-_, probs = model.detect_language(mel)
-print(f"Detected language: {max(probs, key=probs.get)}")
+  # detect the spoken language
+  _, probs = model.detect_language(mel)
+  print(f"Detected language: {max(probs, key=probs.get)}")
 
-# decode the audio
-options = whisper.DecodingOptions(fp16 = torch.cuda.is_available())
-result = whisper.decode(model, mel, options)
+  # decode the audio
+  options = whisper.DecodingOptions(fp16 = torch.cuda.is_available())
+  result = whisper.decode(model, mel, options)
 
-# print the recognized text
-print(result.text)
+  # print the recognized text
+  return result.text
 
-# def voice2text():
-#   print("Voice to Text Converter")
-  
+# if __name__ == "__main__":
+#   print(voice2text())
