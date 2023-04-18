d915c330d2ab0ff9dd2407ecd641e528ca8c4b55
Aryan Chaurasia <aryan6647@gmail.com>
2023-04-18 17:56:38 +0530

    Added Sound Recorder Module

--- main.py
+++ main.py
@@ -59,4 +59,5 @@ def main():
 
 
 if __name__ == "__main__":
-  main()
+  while True:
+    main()
---
+++ modules/voicerecorder.py
@@ -0,0 +1,18 @@
+import wavio as wv
+import sounddevice as sd
+
+# Sampling frequency
+freq = 44100
+
+# Recording duration
+duration = 10
+
+# Recording Settings
+recording = sd.rec(int(duration * freq), samplerate=freq, channels=2)
+
+# Start Recording
+print("STARTED: Voice Recorder")
+sd.wait()
+
+# Save Recording
+wv.write("audio/recording.wav", recording, freq, sampwidth=2)
